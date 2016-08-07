4d95d6ac962d370945a480932f6c77ad940d5110
ScofieldPeng <penjianfeng@gmail.com>
2016-08-07 10:52:23 +0800

    新增对echo框架的一些返回值和请求值得封装

--- echotool/context.go
+++ echotool/context.go
@@ -1,29 +1,35 @@
 package echotool
 
 import (
-	"encoding/json"
-	"github.com/scofieldpeng/echo"
+	"github.com/labstack/echo"
 	"io/ioutil"
+	"github.com/pquerna/ffjson/ffjson"
 )
 
-// GetBody 返回request请求中body的内容,如果出错,第二个参数为error
-func GetBody(ctx *echo.Context) ([]byte, error) {
-	return ioutil.ReadAll(ctx.Request().Body)
+// GetBody 获取请求的body
+func GetBody(ctx echo.Context) (string,error) {
+	bodyByte,err := ioutil.ReadAll(ctx.Request().Body())
+	if err != nil {
+		return "",err
+	}
+
+	return string(bodyByte),nil
 }
 
-// ParseBodyToStruct 将body的内容解析到ouput中,如果出错,返回error
-func ParseBodyToStruct(ctx *echo.Context, output interface{}) error {
-	body, err := GetBody(ctx)
+// GetBodyStruct 获取请求的body体并且将其解析成struct
+func GetBodyStruct(ctx echo.Context,bodyStruct interface{}) error {
+	body,err := GetBody(ctx);
 	if err != nil {
 		return err
 	}
-	if len(body) == 0 {
+
+	if body == "" {
 		return nil
 	}
 
-	return json.Unmarshal(body, output)
-}
+	if err := ffjson.Unmarshal([]byte(body),bodyStruct);err != nil {
+		return err
+	}
 
-// GetMultiForm 获取多媒体文件 TODO
-func GetMultiForm(ctx *echo.Context) {
+	return nil
 }
--- tools/tools.go
+++ tools/tools.go
@@ -35,3 +35,8 @@ func RandomString(lenght ...int) string {
 
 	return res[0:lenght[0]]
 }
+
+// AppDir 应用目录绝对路径
+func AppDir() string{
+	return filepath.Dir(os.Args[0])
+}
